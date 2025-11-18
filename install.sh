d16a214aea1cdcace986c2de6d2cc0921cdbe1fb
Anthony Woodward <anthony.s.woodward1@gmail.com>
2025-11-17 23:26:28 -0500

    added root privileges to commands, created readme file

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
+#!/usr/bin/env bash
+#
+# Author  : Anthony Woodward
+# Date    : 17 November 2025
+# Updated : 17 November 2025
+
+#update repositories
+    sudo apt update
 #install base tools
-    apt install \
+    sudo apt install \
     build-essential \
     cmake \
     curl \
@@ -17,18 +25,17 @@
     tree \
     -y
 #install steam
-    apt install steam -y
+    sudo snap install steam -y
 #install heroic games launcher
     wget https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.18.1/Heroic-2.18.1-linux-amd64.deb
-    sudo dpkg -i Heroic*amd64.deb
+    sudo dpkg -i Heroic*amd64.deb -y
 #install lutris
     echo -e "Types: deb\nURIs: https://download.opensuse.org/repositories/home:/strycore/Debian_12/\nSuites: ./\nComponents: \nSigned-By: /etc/apt/keyrings/lutris.gpg" | sudo tee /etc/apt/sources.list.d/lutris.sources > /dev/null
     wget -q -O- https://download.opensuse.org/repositories/home:/strycore/Debian_12/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/lutris.gpg
     apt update
-    apt install lutris
+    apt install lutris -y
 #install Discord
-    wget https://discord.com/api/download?platform=linux
-    apt install ./discord* -y
+    sudo snap install discord -y
 #install google chrome
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo dpkg -i google-chrome-stable_current_amd64.deb
+    sudo dpkg -i google-chrome-stable_current_amd64.deb -y
