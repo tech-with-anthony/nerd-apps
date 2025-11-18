619bccc14cf42fc1561b6e4fcd6c9fd5b9388bcb
Anthony Woodward <anthony.s.woodward1@gmail.com>
2025-11-17 23:27:44 -0500

    added final sudo

--- install.sh
+++ install.sh
@@ -32,8 +32,8 @@
 #install lutris
     echo -e "Types: deb\nURIs: https://download.opensuse.org/repositories/home:/strycore/Debian_12/\nSuites: ./\nComponents: \nSigned-By: /etc/apt/keyrings/lutris.gpg" | sudo tee /etc/apt/sources.list.d/lutris.sources > /dev/null
     wget -q -O- https://download.opensuse.org/repositories/home:/strycore/Debian_12/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/lutris.gpg
-    apt update
-    apt install lutris -y
+    sudo apt update
+    sudo apt install lutris -y
 #install Discord
     sudo snap install discord -y
 #install google chrome
