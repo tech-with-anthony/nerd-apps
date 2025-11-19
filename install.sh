a3b31003c5086465a8357c9b08bf52168da73311
Anthony <anthony.s.woodward1@gmail.com>
2025-11-18 23:33:56 -0500

    updated insall script
    Split installation of packages into individual commands for clarity and removed unnecessary '-y' flag from dpkg commands.

--- install.sh
+++ install.sh
@@ -7,29 +7,27 @@
 #update repositories
     sudo apt update
 #install base tools
-    sudo apt install \
-    build-essential \
-    cmake \
-    curl \
-    git \
-    gpg \
-    imagemagick \
-    jq \
-    net-tools \
-    openssh-server \
-    screen \
-    socat \
-    steghide \
-    stow \
-    xsel \
-    tree \
-    snapd \
-    -y
+    sudo apt install build-essential -y
+    sudo apt install cmake -y
+    sudo apt install curl -y
+    sudo apt install git -y
+    sudo apt install gpg -y
+    sudo apt install imagemagick -y
+    sudo apt install jq -y
+    sudo apt install net-tools -y
+    sudo apt install openssh-server -y
+    sudo apt install screen -y
+    sudo apt install socat -y
+    sudo apt install steghide -y
+    sudo apt install stow -y
+    sudo apt install xsel -y
+    sudo apt install tree -y
+    sudo apt install snapd -y
 #install steam
     sudo snap install steam -y
 #install heroic games launcher
     wget https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.18.1/Heroic-2.18.1-linux-amd64.deb
-    sudo dpkg -i Heroic*amd64.deb -y
+    sudo dpkg -i Heroic*amd64.deb
 #install lutris
     echo -e "Types: deb\nURIs: https://download.opensuse.org/repositories/home:/strycore/Debian_12/\nSuites: ./\nComponents: \nSigned-By: /etc/apt/keyrings/lutris.gpg" | sudo tee /etc/apt/sources.list.d/lutris.sources > /dev/null
     wget -q -O- https://download.opensuse.org/repositories/home:/strycore/Debian_12/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/lutris.gpg
@@ -39,6 +37,6 @@
     sudo snap install discord -y
 #install google chrome
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo dpkg -i google-chrome-stable_current_amd64.deb -y
+    sudo dpkg -i google-chrome-stable_current_amd64.deb
 #install vs code
     sudo snap install code --classic -y
