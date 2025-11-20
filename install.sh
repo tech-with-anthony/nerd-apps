9191b134925b7a562a229c3c76848ac5e8bc0565
Anthony Woodward <anthony.s.woodward1@gmail.com>
2025-11-19 21:29:00 -0500

    temp comment out of lutris install due to errors. removed -y command for snapd installs

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@
     sudo snap install steam
 #install heroic games launcher
     wget https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.18.1/Heroic-2.18.1-linux-amd64.deb
-    sudo dpkg -i Heroic*amd64.deb
+    sudo dpkg -i Heroic*amd64.deb -y
 #install lutris
     echo -e "Types: deb\nURIs: https://download.opensuse.org/repositories/home:/strycore/Debian_12/\nSuites: ./\nComponents: \nSigned-By: /etc/apt/keyrings/lutris.gpg" | sudo tee /etc/apt/sources.list.d/lutris.sources > /dev/null
     wget -q -O- https://download.opensuse.org/repositories/home:/strycore/Debian_12/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/lutris.gpg
@@ -39,4 +39,4 @@
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo dpkg -i google-chrome-stable_current_amd64.deb
 #install vs code
-    sudo snap install code --classic
+    sudo snap install code --classic -y
