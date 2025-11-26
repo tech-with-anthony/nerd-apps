1b3e78ccd80bea07a09a80c3b94b4a715664fb9b
Anthony <anthony.s.woodward1@gmail.com>
2025-11-25 22:23:17 -0500

    Updated readme file, added install scripts between deb packages, flatpak, and snapd for game launchers, emails client, notes app, chat services

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-<h1 style="font-weight: bold; text-decoration: underline;">Nerd Apps</h1>
+<h1 style="font-weight: bold; text-decoration: underline; text-align: center">Nerd Apps</h1>
 
 <h3>All-in-one script for installing popular "nerdy apps"</h3>
 
 <p>All the favorite apps in one place for Debian based systems. Please open a ticket if any bugs are found or recommendations for additional software to be added.</p>
+
+_Anthony_
--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 #
 # Author  : Anthony Woodward
 # Date    : 17 November 2025
-# Updated : 17 November 2025
+# Updated : 25 November 2025
 
 #update repositories
     sudo apt update
@@ -23,20 +23,29 @@
     sudo apt install xsel -y
     sudo apt install tree -y
     sudo apt install snapd -y
-#install steam
-    sudo snap install steam
-#install heroic games launcher
-    wget https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.18.1/Heroic-2.18.1-linux-amd64.deb
-    sudo dpkg -i Heroic*amd64.deb
-#install lutris (still needs work before going live. repo returns unauthenticated key)
-    # echo -e "Types: deb\nURIs: https://download.opensuse.org/repositories/home:/strycore/Debian_12/\nSuites: ./\nComponents: \nSigned-By: /etc/apt/keyrings/lutris.gpg" | sudo tee /etc/apt/sources.list.d/lutris.sources > /dev/null
-    # wget -q -O- https://download.opensuse.org/repositories/home:/strycore/Debian_12/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/lutris.gpg
-    # sudo apt update
-    # sudo apt install lutris -y
-#install Discord
-    sudo snap install discord
-#install google chrome
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo dpkg -i google-chrome-stable_current_amd64.deb
-#install vs code
-    sudo snap install code --classic
+    sudo apt install neofetch -y
+#install flatpak tools
+    sudo apt install flatpak -y
+    sudo apt install gnome-software-plugin-flatpak -y
+    flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+####Install Flatpak related apllications####
+    #install evolution email client    
+        flatpak install flathub org.gnome.Evolution -y
+    #install obsidian notes app
+        flatpak install flathub md.obsidian.Obsidian -y
+####Install deb package applications####
+    #install heroic games launcher
+        wget https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.18.1/Heroic-2.18.1-linux-amd64.deb
+        sudo dpkg -i Heroic*amd64.deb
+    #install google chrome
+        wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+        sudo dpkg -i google-chrome-stable_current_amd64.deb
+####Install snap package applications####
+    #install steam
+        sudo snap install steam
+    #install Discord
+        sudo snap install discord
+    #install vs code
+        sudo snap install code --classic
+    #install teams-for-linux
+        sudo snap install teams-for-linux
